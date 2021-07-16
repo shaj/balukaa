6278c8fec3fb8fb87179a682eb47949d4f345a18
ShajTester <kolobrod2002@yandex.ru>
2021-07-16 15:57:00 +0300

    Move to pg

--- balukaa/balukaa/settings.py
+++ balukaa/balukaa/settings.py
@@ -84,13 +84,26 @@ WSGI_APPLICATION = "balukaa.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': os.environ.get("POSTGRES_USER"),
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': 'pg',
+        'PORT': '5432',
+        'TEST': {
+            'NAME': 'mytestdatabase',
+        },
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
--- balukaa/entrypoint.sh
+++ balukaa/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+python manage.py wait_for_db
+
 echo "Run migrations"
 python manage.py migrate
 
